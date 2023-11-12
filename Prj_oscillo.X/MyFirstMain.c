174b0742a4fe24815d22fbc7812e357a2c32eca3
theworm <antoine.debrou@edu.ece.fr>
2023-11-12 17:42:32 +0100

    Commit the multimeter part with 7-seg displaying

--- Prj_oscillo.X/MyFirstMain.c
+++ Prj_oscillo.X/MyFirstMain.c
@@ -1,91 +1,222 @@
-/* 
+/*
  * File:   newmain.c
- * Author: theworm
+ * Author: Adrian Chazottes
  *
- * Created on 25 octobre 2023, 11:39
+ * Created on 18 novembre 2016, 10:00
  */
 
-#include<xc.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include"glcd.h" // library for glcd screen
+#include "main.h"
 
-#define _XTAL_FREQ 8000000
-#pragma config WDT = OFF // watchdogOFF
-#pragma config FOSC = HS // Oscillator
-
-
-/* Just prototypes */
-void initMyPIC18F(void);
-void PORTB_Interrupt_ISR(void);
-void __interrupt() irq_handle(void);
-void toggle(void);
-void MyHelloWorld(void);
+// for debug
+#include <string.h>
 /*
+#pragma config FOSC = HS 		//oscillator HS
+#pragma config PWRT = OFF
+#pragma config BOR = OFF
+#pragma config WDT = OFF 		//Disable watchDog
+#pragma config LVP = OFF 		//Disable low voltage programmig
+//#pragma config DEBUG = ON		//Debug ON
+*/
+
+//#pragma config FOSC = HS // use for High Speed crystal Resonator
+
+/*
+ * DEFINE FOR THE GRID
+ */
+#define x_start             2
+#define x_end               (LCD_WIDTH - 2)
+#define y_start             1
+#define y_end               (LCD_HEIGHT - 3)
+
+#define GRID_config          0
+
+#define max_value_converter (uint32_t) 1023
+
+uint16_t point_to_print; // global value to print
+unsigned char index_glcd;
+unsigned char mode = 1;
+unsigned char mode_reglage = 0;
+float amplitude = 0;
+
+
+/*
+ * Pour zoomer dans la courbe (variation temporel) il faut augmenter ou réduire 
+ * le délais.
+ * Augmenter le délais va permettre de dézoomer la courbe, mais on verra plus
+ * d'écart entre les points: il faut enregistrer les points et les écrires dans 
+ * le glcd tout les Nombre_de_point_enregistrer/nombre_de_point_a_afficher.
+ * Il faut appliquer cette méthode dès que l'écart entre les points devient
+ * trop important (Ce nombre de point peut varier selon la fréquence d'entrée ?)
+ * Pour dézoomer il suffit de réduire le délais
+ * 
+ * Pour varier en amplitude, il suffit de brancher un potentiomètre à la valeur
+ * max utilisé pour convertir la source en millivolt.
+ * 
+ * Pour ajouter un offset il faut faire attention à la valeur maximum et
+ * convertir entre 0 et (max - offset) sinon la courbe overflow.
  * 
  */
-int main(int argc, char** argv) {
-    initMyPIC18F();
-    PORTCbits.RC1 = 0;
+
+void main (void)
+{
+    // call the function that will init the PIC
     
-   // MyHelloWorld();
+    initMyPIC18F();
+    glcd_Init(GLCD_ON);
+    
+    glcd_text_write("Hello there",5,5);
+    __delay_us(1000000);
+    init_screen_for_oscilloscope(GRID_config); // 0 = no GRID
+//    print_curve(point_to_print);
+
     while(1)
     {
-        toggle();
+        __delay_us(20000);
+        ADCON0bits.GO = 1; //start converter
     }
-    return (EXIT_SUCCESS);
+
+}// end of the main function
+
+// make sure to have an empty LAST line in any *.c file (just hit an Enter)!
+
+
+
+void print_curve(uint16_t point){
+    unsigned char x;
+            float y;
+    point_to_print = point;
+    convert_for_oscilloscope();
+    y = y_end - ((unsigned char) point_to_print * amplitude) ;
+    // même chose
+    if ( y < y_start){
+        glcd_text_write((unsigned char*)"Overflow !!!!",5,5);
+        return;
+    }
+    
+    x = index_glcd;
+    if(mode == 0){
+        if(index_glcd <= x_end){
+            glcd_PlotPixel( x  , (unsigned char) y   , 0xFF);
+            index_glcd++;
+        }  
+    }else{
+        glcd_PlotPixel( x  , (unsigned char) y   , 0xFF);
+        index_glcd++;
+        if(index_glcd >= x_end){
+            init_screen_for_oscilloscope(GRID_config);    
+        }
+    }        
+    
+    return;
 }
 
- void initMyPIC18F(void){
-    // configure RC0 as a button to change RB0's state
-   
-     //TRISCbits.TRISC1= 0; //RB0 isan OUTPUT
-    
-    // configure interrupton PORTB to light ON RE0
-    glcd_Init(GLCD_ON); // Init function for GLCF screen
-    
-    INTCONbits.GIE= 1;
-    INTCONbits.RBIE= 1;
-    /*
-    TRISBbits.TRISB4= 1; // RB4 isan INPUT
-    TRISBbits.TRISB5= 1; // RB5 isan INPUT
-    TRISBbits.TRISB6= 1; // RB6 isan INPUT
-    TRISBbits.TRISB7= 1; // RB7 isan INPUT
-    TRISEbits.TRISE0 = 0; // RE0 isan OUTPUT
- */
-}
- /*
- void PORTB_Interrupt_ISR(void){
-    INTCONbits.RBIF= 0;
-    // light on RE0 for 1 sec
-    PORTEbits.RE0= 1;
-    __delay_ms(100);
-    PORTEbits.RE0= 0;
- }
 
- void __interrupt() irq_handle(void){
-    if(INTCONbits.RBIF == 1) {
-            PORTB_Interrupt_ISR();
-    }
+// TEST A FAIRE SUR LA CONVERSION (IF > 1023 et vérifier > 5000) + print
+// sur l'écran la valeur à chaque étape.
+// convert from [0 : 5000] to [0 : 61]
+void convert_for_oscilloscope(void){
+    decimal_to_millivolt();
+    // convert from millivot to printable value
+    uint32_t buffer_temp = 0;
+    buffer_temp = (uint32_t) point_to_print * (uint32_t) 61 / 5000 ; 
+    point_to_print = (uint16_t) buffer_temp;
+    return;
 }
-*/ 
- void toggle(void)
- {
-//    if(PORTCbits.RC0 == 1){ 
-//        PORTBbits.RB0 = !PORTBbits.RB0;
-        PORTCbits.RC1 = 0;
-     //  }
-        __delay_ms(1000); // 1000 ms delay
-        PORTCbits.RC1 = 1;
-        __delay_ms(1000); // 1000 ms delay
- }
- 
- void MyHelloWorld(void){
-     unsigned char x , y;
-     char Table[11] = "Hello World";
-     
-    // for(int i = 0 ; i < 3 ; i ++){  
-     //}
-     glcd_text_write(Table, 20, 20);    
-     
- }
+
+
+// convert from [0 : 1023] to [0 : 5000]
+void decimal_to_millivolt(void){
+    uint32_t buffer_temp; //avoid overflow
+    buffer_temp =  ((uint32_t)point_to_print) *((uint32_t)5000)/1023;
+    point_to_print  = (uint16_t) buffer_temp;
+    return;
+}
+void single_mode_manager(void){
+    mode = !mode;
+    init_screen_for_oscilloscope(GRID_config);        
+    return;
+}
+
+void ADC_mode(uint16_t receiveADC){
+    if(mode_reglage == 1){
+        // mode to use potar for amplitude or temporal modification
+            mode_reglage = !mode_reglage;
+            amplitude = (float) receiveADC * ( float )1.2 / 1023; 
+            //set mode for the next acquisition to receive the input signal
+            TRISEbits.TRISE1 = 1; //AN6/RE1 INPUT FOR ADC Converter
+            ADCON0 = 0b00011001; // Channel AN6 selected and converter enable
+            ADCON1 = 0b00001000; // 1000 = DAAAAAAA
+            
+    }else{
+        // mode to received the input in AN6 => PIN RE1
+            mode_reglage = !mode_reglage; 
+            
+            /* set mode for the next acquisition for amplitude and temporal 
+             * modification
+             */
+            TRISAbits.TRISA0 = 1; //AN0 INPUT FOR ADC Converter
+            ADCON0 = 0b00000001; // Channel AN0 selected and converter enable
+            ADCON1 = 0b00001110; // 1110 = DDDDDDDA
+            
+            print_curve(receiveADC);
+        }
+}
+
+
+void init_screen_for_oscilloscope(char grid ){ // ajout de paramètre grille ON/OFF
+    glcd_FillScreen(0);
+    print_axis();    
+    index_glcd = x_start;
+    if(grid){
+     print_grid();   
+    }
+    if(mode==0){
+        for(int i = x_start ; i < x_end-5 ; i+=2){
+            glcd_PlotPixel(i,63,0xFF); //marks for single mode
+        }
+    }
+    return;
+}
+
+
+void print_axis(void){
+    unsigned char x,y;
+    
+    //print arrow for axis y
+    x = x_start;
+    y = y_start; 
+    glcd_PlotPixel(x-2, y+2, 0xFF);
+    glcd_PlotPixel(x-1, y+1, 0xFF);
+    glcd_PlotPixel(x+1, y+1, 0xFF);
+    glcd_PlotPixel(x+2, y+2, 0xFF);
+    
+    
+    // print axis Y
+    x = x_start;
+    for(y = y_start ; y < y_end ; y ++){
+        glcd_PlotPixel(x, y, 0xFF);
+    }
+    //print arrow for axis X
+    x = x_end;
+    y = y_end;
+    glcd_PlotPixel(x-2, y-2, 0xFF);
+    glcd_PlotPixel(x-1, y-1, 0xFF);
+    glcd_PlotPixel(x, y, 0xFF);
+    glcd_PlotPixel(x-1, y+1, 0xFF);
+    glcd_PlotPixel(x-2, y+2, 0xFF);
+    
+    // print axis X
+    y = y_end;
+    for(x = x_start ; x < x_end ; x ++){
+        glcd_PlotPixel(x, y, 0xFF);
+    }
+    
+}
+
+void print_grid(void){
+    unsigned char x,y;
+    
+    glcd_PlotPixel(x, y, 0xFF);
+
+
+}
--- Prj_oscillo.X/glcd.h
+++ Prj_oscillo.X/glcd.h
@@ -1,16 +1,12 @@
-#include <pic18f4550.h>
-#include <xc.h>
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
-
-#ifndef _GLCD_H_
-#define _GLCD_H_
-
-/*============================================================================*
- *                                 GLCD Module                                *
- *============================================================================*/
+/* 
+ * File:   glcd.h
+* Author: Adrian Chazottes
+ *
+ * Created on 18 novembre 2016, 10:00
+ */
+//#include <ctype.h>
+#ifndef GLCD_H
+#define	GLCD_H
 
 #define 	LCD_WIDTH			128
 #define 	LCD_HEIGHT			64
@@ -23,12 +19,12 @@
 #define 	GLCD_RST			LATBbits.LATB5
 
 #define 	GLCD_DATA_TRIS		TRISD
-#define 	WR_DATA				LATD
-#define 	RD_DATA				PORTD
+#define 	WR_DATA				LATD		
+#define 	RD_DATA				PORTD	
 
 #define 	GLCD_LEFT   	 	0
-#define 	GLCD_RIGHT  	 	1
-
+#define 	GLCD_RIGHT  	 	1	
+	
 #define 	GLCD_OFF			0
 #define 	GLCD_ON				1
 
@@ -38,20 +34,26 @@
 #define		f3X6				0
 #define		f8X8				1
 
+#define XTAL_FREQ 8000000 // oscillator frequency for _delay()
 
+#define _XTAL_FREQ 8000000 // oscillator frequency for _delay_ms()
 
-extern void glcd_Init(unsigned char mode);
-extern void glcd_WriteByte(unsigned char side, unsigned char data);
-extern unsigned char glcd_ReadByte(unsigned char side);
-extern void glcd_PlotPixel(unsigned char x, unsigned char y, unsigned char color);
-extern void glcd_SetCursor(unsigned char xpos,unsigned char ypos);
-void glcd_Rect(unsigned char xs, unsigned char ys, unsigned char xe ,unsigned char ye,unsigned char color);
-extern void glcd_FillScreen(unsigned char color);
-extern void glcd_WriteChar8X8( unsigned char ch, unsigned char color);
-extern void glcd_WriteChar3x6( unsigned char ch, unsigned char color);
-extern void glcd_WriteString(const char str[],unsigned char font,unsigned char color);
-extern void glcd_Image(void);
-void glcd_text_write(const char str[], unsigned char x, unsigned char y);
+#include <htc.h>
 
-#endif /* ! _GLCD_H_ */
+//************************************************************************
+// PROTOTYPES
+//************************************************************************
+void glcd_Init(unsigned char mode);
+void glcd_WriteByte(unsigned char side, unsigned char data);
+unsigned char glcd_ReadByte(unsigned char side);
+void glcd_PlotPixel(unsigned char x, unsigned char y, unsigned char color);
+void glcd_SetCursor(unsigned char xpos,unsigned char ypos);
+void glcd_FillScreen(unsigned char color);
+void glcd_WriteChar8X8( unsigned char ch, unsigned char color);
+void glcd_WriteChar3x6( unsigned char ch, unsigned char color);
+void glcd_WriteString(unsigned char str[],unsigned char font,unsigned char color);
+void glcd_Image(void);
+void glcd_text_write(unsigned char str[], unsigned char x, unsigned char y);
+
+#endif	/* GLCD_H */
 
